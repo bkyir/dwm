e899bb728fcd51d7b5c78a56a5fac3701586bf9c
xhzheng <r03z02@163.com>
2024-08-22 23:42:21 +0800

    add autostart

---
+++ autostart/copy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf ~/.local/share/dwm/*
+cp ./* ~/.local/share/dwm/
---
+++ autostart/date.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+while true; do
+    var _date = "$(date)"
+	xsetroot -name $_date
+	sleep 2
+done
